0980583e937320e5dc83d979b652a2b914a3b93a
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-08-24 18:00:11 -0500

    First Routes on App
    Had to uninstall react-router and install react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // TODO: new logos in /public/logo###.png
 
 function App() {
   return (
-    <div className="App">
-    {/* TODO: Header component */}
+    <Router>
+      <div className="App">
+      {/* TODO: Header component */}
 
-    <main>
-      Testing
-    </main>
+      <main>
+        <Switch>
+          <Route path='/'>
+            {/* TODO Mainpage Component */}
+            <div>
+              <p>This is the index page</p>
+            </div>
+          </Route>
+          <Route path="/about">
+            <div>
+              <p>About Page</p>
+            </div>
+          </Route>
+        </Switch>
+      </main>
 
-    {/* TODO: Footer component */}
-    </div>
+      {/* TODO: Footer component */}
+      </div>
+    </Router>
   );
 }
 
