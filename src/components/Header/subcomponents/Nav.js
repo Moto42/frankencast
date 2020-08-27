886217c0c701d48b971b6fd10e27cdedcabe9d57
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-08-27 09:49:14 -0500

    Todo for new feature

--- src/components/Header/subcomponents/Nav.js
+++ src/components/Header/subcomponents/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+// TODO: have the navigation links indicate where you are in the app
 function Nav(props) {
   return(<div>
     <Link to="/">Home</Link>
